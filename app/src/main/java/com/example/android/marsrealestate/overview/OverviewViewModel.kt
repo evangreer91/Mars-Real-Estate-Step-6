f0bf71867166ba06850a13dee5facdb8f30fa741
Evan Greer <evan.greer@iterable.com>
2022-08-24 15:05:50 -0600

    adds error handling and associated image view

---
+++ app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2018, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.example.android.marsrealestate.overview
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.android.marsrealestate.network.MarsApi
+import com.example.android.marsrealestate.network.MarsProperty
+//import kotlinx.coroutines.CoroutineScope
+//import kotlinx.coroutines.Dispatchers
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+// TODO (01) Create a MarsApiStatus enum with the LOADING, ERROR, and DONE states
+
+// we want to add some basic error handling into the app so the user has a better idea of what happened
+// if the internet is not available, we will show our connection error icon and will show the loading animation while fetching the Mars property list
+
+// we need to create a liveData in our view model to represent the status of our web request
+
+enum class MarsApiStatus { LOADING, ERROR, DONE }
+
+/**
+ * The [ViewModel] that is attached to the [OverviewFragment].
+ */
+class OverviewViewModel : ViewModel() {
+
+    //we change the status to type MarsApiStatus
+
+    // The internal MutableLiveData String that stores the most recent response status
+    private val _status = MutableLiveData<MarsApiStatus>()
+
+    // The external immutable LiveData for the status String
+    val status: LiveData<MarsApiStatus>
+        get() = _status
+
+    // Internally, we use a MutableLiveData, because we will be updating the List of MarsProperty
+    // with new values
+    private val _properties = MutableLiveData<List<MarsProperty>>()
+
+    // The external LiveData interface to the property is immutable, so only this class can modify
+    val properties: LiveData<List<MarsProperty>>
+        get() = _properties
+
+    /**
+     * Call getMarsRealEstateProperties() on init so we can display status immediately.
+     */
+    init {
+        getMarsRealEstateProperties()
+    }
+
+    /**
+     * Gets Mars real estate property information from the Mars API Retrofit service and updates the
+     * [MarsProperty] [List] [LiveData]. The Retrofit service returns a coroutine Deferred, which we
+     * await to get the result of the transaction.
+     */
+    private fun getMarsRealEstateProperties() {
+        // update coroutine calls to update status accordingly
+        viewModelScope.launch {
+            _status.value = MarsApiStatus.LOADING
+            try {
+                _properties.value = MarsApi.retrofitService.getProperties()
+                _status.value = MarsApiStatus.DONE
+            } catch (e: Exception) {
+                _status.value = MarsApiStatus.ERROR
+                _properties.value = ArrayList()
+            }
+        }
+    }
+
+    /**
+     */
+}
